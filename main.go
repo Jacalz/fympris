1605de1ff3f46d1df30294201bdeeb94c77993eb
Jacalz <Jacalz@users.noreply.github.com>
2023-04-16 10:45:01 +0200

    Get the interface code mostly finished

--- main.go
+++ main.go
@@ -1,34 +1,93 @@
 package main
 
 import (
-	"log"
+	"errors"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 
 	"github.com/Pauloo27/go-mpris"
 	"github.com/godbus/dbus/v5"
 )
 
-func initMRPISPlayer() *mpris.Player {
+func initMRPISPlayer() (*mpris.Player, error) {
 	conn, err := dbus.SessionBus()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	names, err := mpris.List(conn)
 	if err != nil {
-		panic(err)
-	}
-	if len(names) == 0 {
-		log.Fatal("No player found")
+		return nil, err
 	}
 
-	name := names[0]
-	return mpris.New(conn, name)
+	if len(names) == 0 {
+		return nil, errors.New("no player found")
+	}
+
+	return mpris.New(conn, names[0]), nil
 }
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("MPRIS")
 
+	player, err := initMRPISPlayer()
+	if err != nil {
+		fyne.LogError("Failed to get player", err)
+		return
+	}
+
+	previous := &widget.Button{Icon: theme.MediaSkipPreviousIcon(), OnTapped: func() {
+		err := player.Previous()
+		if err != nil {
+			fyne.LogError("Could not skip to previous", err)
+		}
+	}}
+
+	status, err := player.GetPlaybackStatus()
+	if err != nil {
+		fyne.LogError("Failed to get playback status", err)
+		return
+	}
+
+	icon := theme.MediaPlayIcon()
+	if status == mpris.PlaybackPaused {
+		icon = theme.MediaPlayIcon()
+	}
+
+	playOrPause := &widget.Button{Icon: icon}
+	playOrPause.OnTapped = func() {
+		err := player.PlayPause()
+		if err != nil {
+			fyne.LogError("Could not change playback mode", err)
+			return
+		}
+
+		status, err := player.GetPlaybackStatus()
+		if err != nil {
+			fyne.LogError("Failed to get playback status", err)
+			return
+		}
+
+		if status == mpris.PlaybackPaused {
+			playOrPause.Icon = theme.MediaPlayIcon()
+		} else {
+			playOrPause.Icon = theme.MediaPlayIcon()
+		}
+
+		playOrPause.Refresh()
+	}
+
+	next := &widget.Button{Icon: theme.MediaSkipNextIcon(), OnTapped: func() {
+		err := player.Next()
+		if err != nil {
+			fyne.LogError("Could not skip to next", err)
+		}
+	}}
+
+	w.SetContent(container.NewHBox(previous, playOrPause, next))
 	w.ShowAndRun()
 }
